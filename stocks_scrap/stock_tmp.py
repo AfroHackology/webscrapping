f89f6b3b2bf39292da5e20d59b7b2ad40532a63d
Gregory James <gregoryjms7@gmail.com>
2019-05-09 23:17:26 -0700

    Edit stock_tmp

--- stocks_scrap/stock_tmp.py
+++ stocks_scrap/stock_tmp.py
@@ -1,4 +1,4 @@
-from urllib.request import *
+import urllib.request
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
@@ -6,20 +6,22 @@ from datetime import datetime
 # specify the url
 quote_page = 'https://www.bloomberg.com/quote/SPX:IND'
 # query the website and return the html to the variable 'page'
-page = urlopen(quote_page)
+page = urllib.request.urlopen(quote_page)
 # parse the html using beautiful soup and store in variable 'soup'
 soup = BeautifulSoup(page, 'html.parser')
 
 # Take out the <div> of name and get its value
 name_box = soup.find('h1', attrs={'class': 'name'})
+
 # Get the data by getting its text
-name = name_box.str.strip()
+name = name_box
+
 # strip() is used to remove starting and trailing
 print(name)
 
 # get the index price
 price_box = soup.find('div', attrs={'class': 'price'})
-price = price_box.text
+price = price_box
 print(price)
 
 # open a csv file with append, so old data will not be erased
